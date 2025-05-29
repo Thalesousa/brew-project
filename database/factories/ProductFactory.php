c4624830ba29216cda9d6de4ccab9323f908f7bf
Thalesousa <thalestjsb@gmail.com>
2025-05-28 22:31:30 -0300

    feat: init project

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Product>
+ */
+class ProductFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => 1,
+            'name' => fake()->word(),
+            'sku' => str()->random(8),
+            'image' => null,
+            'is_active' => fake()->boolean(true),
+            'price' => fake()->randomFloat(2, 1, 1000),
+            'stock' => fake()->numberBetween(0, 100),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ];
+    }
+}

cd054a8f70901ad99fb850260caf6e8a29eedef0
Thalesousa <thalestjsb@gmail.com>
2025-05-30 16:16:58 -0300

    update: button layout color and icon

--- resources/views/livewire/product/button-create.blade.php
+++ resources/views/livewire/product/button-create.blade.php
@@ -3,9 +3,14 @@
         type="button"
         class="bg-transparent hover:bg-blue-700 text-blue-600 font-semibold py-1.5 px-4
                   rounded-lg transition duration-300 border-blue-600 border-2 hover:border-transparent
-                  hover:text-white"
+                  hover:text-white cursor-pointer"
         wire:click="toggleModal"
     >
-        Novo Produto
+        <span class="hidden md:inline">Novo Produto</span>
+        <span class="inline md:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+        </span>
     </button>
 </div>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,8 +1,15 @@
 <x-layout title="Produtos">
   <div class="flex flex-col justify-center max-w-7xl mx-auto">
     <div class="flex items-center justify-between w-full py-4">
-      <span class="inline-block">Olá, {{ auth()->user()->name }} </span>
-      <a href="{{ route('logout') }}" class="inline-block ml-auto px-4 py-2 bg-blue-600 rounded-md text-white">Sair</a>
+        <span class="inline-block">Olá, {{ auth()->user()->name }} </span>
+        <a
+            href="{{ route('logout') }}"
+            class="inline-block ml-auto px-4 py-2 bg-blue-600 rounded-md
+                text-white hover:bg-transparent transition duration-300
+                border-2 border-blue-600 hover:text-blue-700"
+        >
+            Sair
+        </a>
     </div>
     <div class="flex items-center justify-between w-full mt-6 mb-10">
         <h2 class="text-2xl font-bold">Lista de Produtos</h2>
